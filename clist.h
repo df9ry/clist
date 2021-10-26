0d9ba13e39d8c31d5967e9ca31fd96ae7ecc9d1f
Tania Hagn <info@taniahagn.de>
2021-10-26 13:52:27 +0200

    Solved problem with pointer arithmetic warning on Android

--- clist.h
+++ clist.h
@@ -45,9 +45,8 @@ extern "C" {
  * under normal circumstances, used to verify that nobody uses
  * non-initialized list entries.
  */
-#define POISON_POINTER_DELTA 0
-#define LIST_POISON1  ((void *) 0x00100100 + POISON_POINTER_DELTA)
-#define LIST_POISON2  ((void *) 0x00200200 + POISON_POINTER_DELTA)
+#define LIST_POISON1  ((void *) 0x00100100)
+#define LIST_POISON2  ((void *) 0x00200200)
 
 #include <stdlib.h>
 #include <stdbool.h>
